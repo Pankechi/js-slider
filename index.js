11f61d79b3a45c43fa283cfe5b4f4f1b389651e6
Aleksey Amozov <spacemarce@gmail.com>
2022-12-16 19:26:49 +0300

    classname fix

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
-'use strict'
-
+"use strict";
 
 //слайдер
-const wrapper = document.querySelector('.slider_wrapper')
+const wrapper = document.querySelector(".need-slider_wrapper");
 
-const slider = document.querySelector('.slider')
+const slider = document.querySelector(".need-slider");
 
-const slide_quantity = slider.children.length
+const slide_quantity = slider.children.length;
 
-const slider_items = document.getElementsByClassName('.slide')
+const slider_items = document.getElementsByClassName(".need-slide");
 
-const sldde_img = document.querySelector('.slide__img')
+const sldde_img = document.querySelector(".need-slide__img");
 
-const slide_img_width = document.querySelector('.slide').getBoundingClientRect().width
+const slide_img_width = document
+  .querySelector(".need-slide")
+  .getBoundingClientRect().width;
 
-const step = slide_img_width
+const step = slide_img_width;
 
-wrapper.style.width = slide_img_width + 'px'
+wrapper.style.width = slide_img_width + "px";
 
+//кнопки
+const next_button = document.querySelector(".need-slider_next");
 
-//кнопки 
-const next_button = document.querySelector('.slider_next')
+const prev_button = document.querySelector(".need-slider_prev");
 
-const prev_button = document.querySelector('.slider_prev')
-
-let position = 0
+let position = 0;
 
 const slideNext = () => {
-    position -= step
+  position -= step;
 
-    if( position <= -(slide_quantity* slide_img_width)) {
-      position = 0
-    }
-
-    slider.style.transform = `translateX(${position}px)`;
-    
-}
-
-next_button.addEventListener('click', slideNext)
-
-const slidePrev = () => {
-  position += step
-
-  if( position >= step) {
-    position = -(slide_quantity * slide_img_width - step)
+  if (position <= -(slide_quantity * slide_img_width)) {
+    position = 0;
   }
 
   slider.style.transform = `translateX(${position}px)`;
-}
+};
 
-prev_button.addEventListener('click', slidePrev)
+next_button.addEventListener("click", slideNext);
+
+const slidePrev = () => {
+  position += step;
+
+  if (position >= step) {
+    position = -(slide_quantity * slide_img_width - step);
+  }
+
+  slider.style.transform = `translateX(${position}px)`;
+};
+
+prev_button.addEventListener("click", slidePrev);
 
 //автоплей
 //  let timer = setInterval(slideNext, 2000) //время в миллисекундах (1 к 1000)
-
 
 //  $(document).ready(() => timer)
